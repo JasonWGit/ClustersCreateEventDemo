ed878e75b09fcbda0d732e41cb1fcbaa1ea6b799
JasonW <jason.wang911666@gmail.com>
2025-04-13 01:23:21 +1000

    Forced commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <Stack>
+        <Stack.Screen name="(app)" options={{ headerShown: false }} />
+      </Stack>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
